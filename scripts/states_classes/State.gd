c2c7d9cccb037ed60f8bc1039ab3ab3bdd3c27d0
kafeleknie <kakelek@wp.pl>
2024-09-11 17:25:12 +0200

    added player prototype

--- scripts/states_classes/State.gd
+++ scripts/states_classes/State.gd
@@ -1,28 +1,26 @@
 extends Node
 class_name State
 
-var state_machine = null
+var state_machine = null #to jest ustawiane automatycznie przez StateMachine
 
-var entity: CharacterBody2D
+var entity: CharacterBody2D #węzeł główny bytu do którego nalezy state
 
 func _ready() -> void:
 	await owner.ready # w sumie to nie wiem jak to działa ale bez tego ne działa
 
 	entity = owner as CharacterBody2D
-	assert(entity != null,"could't find owner in State")
+	assert(entity != null,"could't find owner in State") # zabezpieczenie
 
-#virtualne funkcje poniżej :)
-func handle_input(_event: InputEvent) -> void:
+#klasa dziedzicząca state może korzystać z funckji poniżej, wystarczy dodać im zawartość tzn. stworzyć nową funkcje z taką samą nazwą
+#nie trzeba używac wszystkich
+func update(_delta: float) -> void: # funkcja wykonująca się podczas każdej klatki
 	pass
 
-func update(_delta: float) -> void:
+func physics_update(_delta: float) -> void: # funkcja wykonująca się ileś razy w trakcie sekundy(chyba 60)
 	pass
 
-func physics_update(_delta: float) -> void:
+func enter(_msg := {}) -> void: # funckja wykonująca się po aktywowaniu stanu
 	pass
 
-func enter(_msg := {}) -> void:
-	pass
-
-func exit() -> void:
+func exit() -> void: # funckja wykonująca się po wyłączeniu stanu
 	pass
--- scripts/states_classes/StateMachine.gd
+++ scripts/states_classes/StateMachine.gd
@@ -3,28 +3,28 @@ class_name StateMachine
 
 signal transitioned(state_name: String)
 
-@export var initial_state: NodePath
+@export var initial_state: NodePath #trzeba ręcznie ustawić w inspektorze :( (jeżeli to problem, to możemy zrobić żeby było losowe <3)
 @onready var state: State = get_node(initial_state)
 
 func _ready() -> void:
 	await owner.ready
-	for child in get_children():
+	
+	for child in get_children(): # przypisywanie zmiennej state_machine
 		child.state_machine = self
 	state.enter()
 
-func _unhandled_input(event: InputEvent) -> void:
-	state.handle_input(event)
-
 func _process(delta: float) -> void:
 	state.update(delta)
-
+	
 func _physics_process(delta: float) -> void:
 	state.physics_update(delta)
 
-func transition_to(target_state_name: String, msg: Dictionary = {}) -> void:
+
+func transition_to(target_state_name: String, msg: Dictionary = {}) -> void: # zmiana stanu
 	if not has_node(target_state_name):
 		return
 	state.exit()
 	state = get_node(target_state_name)
 	state.enter(msg)
+	print(state) #mozna usunąć
 	emit_signal("transitioned", state.name)
