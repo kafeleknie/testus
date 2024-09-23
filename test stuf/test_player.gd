eeaee91a41eff21213c4d1e5ace4e4c7ce360e61
kafeleknie <kakelek@wp.pl>
2024-09-23 20:50:17 +0200

    backup

--- test stuf/test_player.gd
+++ test stuf/test_player.gd
@@ -15,29 +15,33 @@ var length: float = 0.0
 
 #swinging properties
 var is_swinging: bool = false
-var angle = 0.0
-var acceleration = 0.0 
-var swing_velocity = 0
-var damping_factor = 0.99
+var angle: float = 0.0
+var acceleration: float = 0.0 
+var swing_velocity: float = 0.0
+var damping_factor: float = 1 
+var prevoius_position: Vector2 = Vector2.ZERO
+
 
 func _physics_process(delta: float) -> void:
 	if is_swinging:
 		handle_swinging(delta)
-	elif is_hook_attached and global_position.distance_to(hook_position) > length:
+	elif is_hook_attached:
 		start_swinging()
-		is_swinging = true
 	else:
 		player_movement(delta)
 		velocity.y += gravity * delta
+	move_and_slide()
 	if Input.is_action_just_pressed("send_hook"):
 		shoot_hook()
+		if(is_swinging):
+			stop_swinging()
+			detach_hook()
 
 # Player movement when not swinging
 func player_movement(delta: float) -> void:
 	velocity.x = SPEED if Input.is_action_pressed("ui_right") else -SPEED  if Input.is_action_pressed("ui_left") else 0
 	if Input.is_action_just_pressed("space") and is_on_floor():
 		velocity.y = JUMP_FORCE
-	move_and_slide()
 
 func shoot_hook() -> void:
 	if not hook_instance:
@@ -46,32 +50,43 @@ func shoot_hook() -> void:
 		owner.add_child(hook_instance)
 		hook_instance.set_player(self)
 
-func handle_swinging(delta: float) -> void:
-	angle = (global_position - hook_position).angle_to(Vector2.DOWN)
-	acceleration = -gravity * sin(angle)
-	swing_velocity += acceleration * delta
-	swing_velocity *= damping_factor
-	var buffer_vector:Vector2 = Vector2(
-		swing_velocity * cos(angle),
-		swing_velocity * -sin(angle)
-	)
-	global_position += buffer_vector * delta
-	restrict_player_movement()
-
-func restrict_player_movement():
-	var direction = global_position - hook_position
+#swinging mechanic
+func restrict_player_distance():
+	var direction: Vector2 = global_position - hook_position
 	direction = direction.normalized() * length
-	global_position = hook_position + direction  
+	global_position = hook_position + direction
 
 func start_swinging() -> void:
-	swing_velocity = sqrt(velocity.x **2 + velocity.y**2) * sign( hook_position.x - global_position.x) 
+	swing_velocity = velocity.length() * sign( hook_position.x - global_position.x) 
 	angle = (global_position - hook_position).angle_to(Vector2.DOWN)
 	var buffer_vector: Vector2 = Vector2(
 		swing_velocity * cos(angle),
 		swing_velocity * -sin(angle)
 	)
-	swing_velocity *= cos(velocity.angle_to(buffer_vector))
-	velocity = Vector2.ZERO
+	velocity = buffer_vector * cos(velocity.angle_to(buffer_vector))
+	is_swinging = true
+
+func handle_swinging(delta: float) -> void:
+	var v_length= velocity.length()
+	angle = (global_position - hook_position).angle_to(Vector2.DOWN)
+	acceleration = (-gravity * sin(angle))/length * delta * 100
+	swing_velocity = sign(swing_velocity) * v_length + acceleration 
+	if Input.is_action_just_pressed("space"):
+		swing_velocity *= 2
+	velocity.x = swing_velocity * cos(angle) 
+	velocity.y = swing_velocity * -sin(angle) 
+
+	
+	restrict_player_distance()
+
+func stop_swinging() -> void:
+	angle = (global_position - hook_position).angle_to(Vector2.DOWN)
+	var buffer_vector: Vector2 = Vector2(
+		swing_velocity * cos(angle),
+		swing_velocity * -sin(angle)
+	)
+	velocity = buffer_vector
+	is_swinging = false
 
 func hook_attached(hook_ref: Area2D)->void:
 	is_hook_attached = true
